d16e04288a42b4d1693e69d6e7f908a3f69b6c9c
Summer <113973444+1975686741@users.noreply.github.com>
2022-12-03 21:00:36 +0800

    Add files via upload

---
+++ js/shouxie/instanceof/2.js
@@ -0,0 +1,10 @@
+/**
+ * @func 判断 left 是否是 right 的实例
+ * @param {*} left :对象
+ * @param {*} right ：对象
+ * @return boolean
+ */
+function myInstanceof(left, right) {
+    // right 只要出现在left原型链的任何一栈都可以
+    // return left instanceof right
+}
