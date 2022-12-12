21062c802b7ca260d353b63cd2c351744bf12004
Summer <113973444+1975686741@users.noreply.github.com>
2022-12-12 18:31:09 +0800

    Add files via upload

---
+++ weapp/shoping/models/detail.js
@@ -0,0 +1,25 @@
+export class GoodsBaseInfo {
+  constructor(itemInfo, columns, services) {
+    // console.log(itemInfo, columns, services);
+    this.title = itemInfo.title
+    this.desc = itemInfo.desc
+    this.newPrice = itemInfo.price
+    this.oldPrice = itemInfo.oldPrice
+    this.discount = itemInfo.discountDesc
+    this.columns = columns
+    this.services = services
+
+    this.realPrice = itemInfo.lowNowPrice
+  }
+}
+
+export class GoodsShopInfo {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score = shopInfo.score;
+    this.goodsCount = shopInfo.cGoods;
+  }
+}
