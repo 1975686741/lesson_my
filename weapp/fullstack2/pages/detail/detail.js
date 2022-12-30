15fbb2cd8bda07d209cf2df0e143744cdc2a7e71
Summer <113973444+1975686741@users.noreply.github.com>
2022-12-30 16:42:09 +0800

    Add files via upload

---
+++ weapp/fullstack2/pages/detail/detail.js
@@ -0,0 +1,81 @@
+// pages/detail/detail.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title: "",
+        desc: ""
+    },
+  
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+      const { id } = options;
+      // console.log(id)
+      this.loadData(id);
+    },
+    loadData(id) {
+      wx.request({
+        url: `https://www.fastmock.site/mock/995214af813bc47208cf2fcb558907e6/mall/detail`,
+        success: (res) => {
+        //   console.log(res);
+            title: res.data.title
+            desc: res.data.desc
+        },
+        fail: (err) => {
+          console.log(err);
+        }
+      })
+    },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+  
+    },
+  
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+  
+    },
+  
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+  
+    },
+  
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+  
+    }
+  })
